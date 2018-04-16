0bbc45fa984ecf3cb228951bda50ada022827729
Krasimir Aleksiev <aleksiev.krasi82@gmail.com>
2018-04-16 15:52:50 +0300

    update packeges and fix animations

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,13 +15,13 @@ import { DataService } from '../data.service';
  	 		query(':enter', stagger('300ms', [
  	 			animate('.6s ease-in', keyframes([
  	 				style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
- 	 				style({opacity: .5, transform: 'translateY(35px)', offset: 0}),
+ 	 				style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
  	 				style({opacity: 1, transform: 'translateY(0)', offset: 1}),
  	 				]))]), {optional: true}),
  	 		query(':leave', stagger('300ms', [
  	 			animate('.6s ease-in', keyframes([
  	 				style({opacity: 1, transform: 'translateY(0)', offset: 0}),
- 	 				style({opacity: .5, transform: 'translateY(50px)', offset: 0}),
+ 	 				style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
  	 				style({opacity: 0, transform: 'translateY(-75%)', offset: 1}),
  	 				]))]), {optional: true})
  	 		])
